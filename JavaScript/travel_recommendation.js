06fad6154093f2d6964e6eeaf500b00788483f79
Hafsa Shahid <hafsashahidchaudhry@example.com>
2025-03-04 18:29:27 +0500

    path change in fetch()

--- JavaScript/travel_recommendation.js
+++ JavaScript/travel_recommendation.js
@@ -1,12 +1,10 @@
 searchButton = document.getElementById("searchButton");
 searchButton.addEventListener("click", fetching);
-
+//https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-JS0101EN-SkillsNetwork/travel1.json
 function fetching() {
   debugger;
   textInput = document.getElementById("searchInput").value.toLowerCase();
-  fetch(
-    "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-JS0101EN-SkillsNetwork/travel1.json"
-  )
+  fetch("./JavaScript/travel_recommendation_api.json")
     .then((response) => {
       return response.json();
     })
